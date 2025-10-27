e4eb8856063d3bdb028df54855e9aa6e920e693f
muli-cohen <mulic3@gmail.com>
2025-10-27 13:22:10 +0200

    add jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,8 @@ export default [
     languageOptions: {
       ecmaVersion: 2021,
       globals: {
-        ...globals.browser
+        ...globals.browser,
+        ...globals.jest
       },
       parserOptions: {
         ecmaFeatures: {
